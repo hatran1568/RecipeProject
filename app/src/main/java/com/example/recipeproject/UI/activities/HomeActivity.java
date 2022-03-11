c304060f758f7e9205e9b230ac05c93ee936643d
Ngoc Anh <ngocanh.a167@gmail.com>
2022-03-12 03:20:05 +0700

    chuyen nha

--- app/src/main/java/com/example/recipeproject/UI/activities/HomeActivity.java
+++ app/src/main/java/com/example/recipeproject/UI/activities/HomeActivity.java
@@ -2,16 +2,48 @@ package com.example.recipeproject.UI.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.recipeproject.DataAccess.DataAccess;
+import com.example.recipeproject.InterfaceGetData.FirebaseCallback;
 import com.example.recipeproject.R;
+import com.example.recipeproject.Repsentation.Adapter;
+import com.example.recipeproject.listener.SelectListener;
+import com.example.recipeproject.model.Recipe;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity implements SelectListener {
+    private Context mContext;
+    private Activity mActivity;
+    RecyclerView recyclerView;
+
+    // Array list for recycler view data source
+    ArrayList<Recipe> source;
+
+    // Layout Manager
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+
+    // adapter class object
+    Adapter adapter;
+
+    // Linear Layout Manager
+    LinearLayoutManager HorizontalLayout;
+
+    View ChildView;
+    int RecyclerViewItemPosition;
+    TextView textView;
 
     BottomNavigationView bottomNavigationView;
 
@@ -44,6 +76,48 @@ public class HomeActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+
+        /// new home page
+        mActivity = HomeActivity.this;
+        mContext = getApplicationContext();
+        textView = findViewById(R.id.newest);
+        DataAccess.getRecipesOnChange(new FirebaseCallback() {
+            @Override
+            public void onResponse(ArrayList<Recipe> recipes) {
+                recyclerView
+                        = (RecyclerView) findViewById(
+                        R.id.recyclerviewNewest);
+                RecyclerViewLayoutManager
+                        = new LinearLayoutManager(
+                        getApplicationContext());
+                // Set LayoutManager on Recycler View
+                recyclerView.setLayoutManager(
+                        RecyclerViewLayoutManager);
+                // calling constructor of adapter
+                // with source list as a parameter
+
+                adapter = new Adapter(recipes,mContext,mActivity,HomeActivity.this);
+                // Set Horizontal Layout Manager
+                // for Recycler view
+                HorizontalLayout
+                        = new LinearLayoutManager(
+                        HomeActivity.this,
+                        LinearLayoutManager.HORIZONTAL,
+                        false);
+                recyclerView.setLayoutManager(HorizontalLayout);
+                // Set adapter on recycler view
+                recyclerView.setAdapter(adapter);
+
+            }
+
+
+        });
+    }
+
+    @Override
+    public void onItemClick(Recipe recipe) {
+
     }
 
 
